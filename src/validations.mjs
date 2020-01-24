c47896157a2c9accc9e51049491b98575f6090a5
buwaro <buwaro@gmail.com>
2020-01-24 14:13:39 +0100

    number validations

--- src/validations.mjs
+++ src/validations.mjs
@@ -1,7 +1,5 @@
 const ean = (v) => {
-  let result = false;
   const length = v.length
-
   const checkSum = v
     .split('')
     .splice(0, length - 1)
@@ -12,11 +10,95 @@ const ean = (v) => {
   const checkDigit = parseInt(v.toString()[length - 1], 10)
   const valid = (10 - (checkSum % 10)) % 10 === checkDigit
 
-  if (length === 18 && valid) { result = true }
+  if (length === 18 && valid) {
+    return true
+  } else {
+    return false
+  }
 
   return result;
 }
 
+const constants = () => {
+  return $constants
+}
+
+const numberPrecision = (v, precision) => {
+  if (
+    v === null ||
+    precision === undefined ||
+    !(v.includes('.') && RegExp(`\\.\\d{${precision + 1}}`).test(v))
+  ) {
+    return true
+  } else {
+    return false
+  }
+}
+
+const numberMaximum = (v, maximum) => {
+  if (
+    v === null ||
+    maximum === undefined ||
+    v <= maximum
+  ) {
+    return true
+  } else {
+    return false
+  }
+}
+
+const numberMinimum = (v, minimum) => {
+  if (
+    v === null ||
+    minimum === undefined ||
+    v >= minimum
+  ) {
+    return true
+  } else {
+    return false
+  }
+}
+
+const numberDot = (v) => {
+  if ( 
+    v === null ||
+    v.match(/\./gi) === null ||
+    v.match(/\./gi).length <= 1
+  ) {
+    return true
+  } else {
+    return false
+  }
+}
+
+const numberMinus = (v) => {
+  if ( 
+    v === null ||
+    v.lastIndexOf('-') <= 0
+  ) {
+    return true
+  } else {
+    return false
+  }
+}
+
+const numberDecimal = (v) => {
+  if ( 
+    v === null ||
+    /^[0-9.\\-]{1,}$/.test(v)
+  ) {
+    return true
+  } else {
+    return false
+  }
+}
+
 export default {
-  ean
+  ean,
+  numberPrecision,
+  numberMaximum,
+  numberMinimum,
+  numberDot,
+  numberMinus,
+  numberDecimal
 }
