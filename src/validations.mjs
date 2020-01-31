4c2393bb4b7fe075321915932d258943e323850a
Oleksii Shaposhnikov <oshap1044@gmail.com>
2020-01-31 09:07:54 +0200

    remarks changes

--- src/validations.mjs
+++ src/validations.mjs
@@ -1,4 +1,8 @@
-const required = (v) => {
+export const string = (v) => {
+  return (typeof v === 'string' && v.length <= 255)
+};
+
+export const required = (v) => {
   return (
     !(typeof v === 'string' && v.trim() === '') &&
     v !== undefined &&
@@ -6,71 +10,67 @@ const required = (v) => {
   )
 };
 
-const string = (v) => {
-  return (typeof v === 'string' && v.length <= 255)
-};
-
-const name = (v) => {
+export const name = (v) => {
   return /^(?=.*[-a-zA-Zа-яА-Я\u00C0-\u017FA])[-a-zA-Zа-яА-Я\u00C0-\u017FA ]+$/.test(v)
 };
 
-const email = (v) => {
+export const email = (v) => {
   return /^[a-zA-Zа-яА-Я0-9.!#$%&’*+/=?^_`{|}~-]+@(?:[a-zA-Zа-яА-Я0-9-]+\.)+[a-zA-Zа-яА-Я0-9-]{2,63}$/.test(v)
 };
 
-const phoneMaximumLength = (v) => {
+export const phoneMaximumLength = (v) => {
   return v.length !== 13
 };
 
-const phonePlus = (v) => {
+export const phonePlus = (v) => {
   return /^\+?[^+]*$/.test(v)
 };
 
-const phoneNoLetters = (v) => {
+export const phoneNoLetters = (v) => {
   return /^[-+()0-9]*$/.test(v)
 };
 
-const domainCharacters = (v) => {
+export const domainCharacters = (v) => {
   return /^[-0-9a-z]+$/.test(v)
 };
 
-const domainLength = (v) => {
+export const domainLength = (v) => {
   return v.length >= 2
 };
 
-const password = (v) => {
+export const password = (v) => {
   return /^(?=.*\d)(?=.*[a-z])(?=.*[A-Z])[\x20-\x7E]{6,32}$/.test(v)
 };
 
-const passwordCharacters = (v) => {
+export const passwordCharacters = (v) => {
   return /^[\x20-\x7E]+$/.test(v)
 };
 
-const passwordMinimumLength = (v) => {
+export const passwordMinimumLength = (v) => {
   return v.length >= 6
 };
 
-const passwordMaximumLength = (v) => {
+export const passwordMaximumLength = (v) => {
   return v.length <= 32
 };
 
-const passwordIncludesDowncase = (v) => {
+export const passwordIncludesDowncase = (v) => {
   return /[a-z]/.test(v)
 };
 
-const passwordIncludesUpcase = (v) => {
+export const passwordIncludesUpcase = (v) => {
   return /[A-Z]/.test(v)
 };
 
-const passwordIncludesNumbers = (v) => {
+export const passwordIncludesNumbers = (v) => {
   return /\d/.test(v)
 };
 
-const alias = (v) => {
+export const alias = (v) => {
   return /[a-z0-9-]/.test(v)
 };
 
-const ean = (v) => {
+export const ean = (v) => {
   const length = v.length
   const checkSum = v
     .split('')
@@ -82,131 +82,67 @@ const ean = (v) => {
   const checkDigit = parseInt(v.toString()[length - 1], 10)
   const valid = (10 - (checkSum % 10)) % 10 === checkDigit
 
-  if (
-    length === 18 &&
-    v.startsWith('54') &&
-    valid
-  ) {
-    return true
-  } else {
-    return false
-  }
-
-  return result;
+  return length === 18 && v.startsWith('54') && valid;
 }
 
-const numberPrecision = (v, precision) => {
-  return (
-    v === null ||
-    precision === undefined ||
-    !(v.includes('.') && RegExp(`\\.\\d{${precision + 1}}`).test(v))
-  )
+export const numberPrecision = (v, precision) => {
+  return !(v.includes('.') && RegExp(`\\.\\d{${precision + 1}}`).test(v))
 }
 
-const numberMaximum = (v, maximum) => {
-  return (
-    v === null ||
-    maximum === undefined ||
-    v <= maximum
-  ) }
-
-const numberMinimum = (v, minimum) => {
-  return (
-    v === null ||
-    minimum === undefined ||
-    v >= minimum
-  )
+export const numberMaximum = (v, maximum) => {
+  return v <= maximum
 }
 
-const numberDot = (v) => {
-  return ( 
-    v === null ||
+export const numberMinimum = (v, minimum) => {
+  return v >= minimum
+}
+
+export const numberDot = (v) => {
+  return (
     v.match(/\./gi) === null ||
     v.match(/\./gi).length <= 1
   )
 }
 
-const numberMinus = (v) => {
+export const numberMinus = (v) => {
   return ( 
     v === null ||
     v.lastIndexOf('-') <= 0
   )
 }
 
-const numberDecimal = (v) => {
-  return ( 
-    v === null ||
-    /^[0-9.\\-]{1,}$/.test(v)
-  )
+export const numberDecimal = (v) => {
+  return /^[0-9.\\-]{1,}$/.test(v)
 }
 
-const numberWhole = (v) => {
+export const numberWhole = (v) => {
   return (v % 1 === 0)
 }
 
-const zipLimit = (v) => {
-  if (/^[0-9]{4}$/.test(v)) {
-    return true
-  } else {
-    return false
-  }
+export const zipLimit = (v) => {
+  return /^[0-9]{4}$/.test(v)
 }
 
-const zipMinimum = (v) => {
+export const zipMinimum = (v) => {
   return numberMinimum(v, 1000)
 }
 
-const vatStartBe0 = (v) => {
+export const vatStartBe0 = (v) => {
   return v.toLowerCase().startsWith('be0')
 }
 
-const vat9Numbers = (v) => {
+export const vat9Numbers = (v) => {
   return !/^be0\d{9}$/gi.test(v)
 }
 
-const vat0 = (v) => {
+export const vat0 = (v) => {
   return /^be0000000000/gi.test(v)
 }
 
-const vatStartEnterprise = (v) => {
+export const vatStartEnterprise = (v) => {
   return /^[0,1]/.test(v)
 }
 
-const vatEnterprise = (v) => {
+export const vatEnterprise = (v) => {
   return !/^[0,1]\d{3}\.\d{3}\.\d{3}$/.test(v)
-}
-
-export default {
-  required,
-  string,
-  name,
-  email,
-  phoneMaximumLength,
-  phoneNoLetters,
-  phonePlus,
-  domainCharacters,
-  domainLength,
-  password,
-  passwordCharacters,
-  passwordMinimumLength,
-  passwordMaximumLength,
-  passwordIncludesDowncase,
-  passwordIncludesUpcase,
-  passwordIncludesNumbers,
-  alias,
-  ean,
-  numberPrecision,
-  numberMaximum,
-  numberMinimum,
-  numberDot,
-  numberMinus,
-  numberDecimal,
-  numberWhole,
-  zipLimit,
-  zipMinimum,
-  vatStartBe0,
-  vat9Numbers,
-  vat0,
-  vatStartEnterprise,
-  vatEnterprise,
 }
